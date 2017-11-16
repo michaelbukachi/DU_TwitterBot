3c8018760526e87083955f7b39770734e9ddf43f
Michael Bukachi <michaelbukachi@gmail.com>
2017-11-17 02:31:06 +0300

    Added dependency Added boilerplate code

--- src/main/java/com/dita/dev/twitterbot/Bot.java
+++ src/main/java/com/dita/dev/twitterbot/Bot.java
@@ -1,5 +1,9 @@
 package com.dita.dev.twitterbot;
 
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
@@ -7,7 +11,6 @@ import twitter4j.conf.ConfigurationBuilder;
 public class Bot {
 
     public static void main(String[] args) {
-        ConfigurationBuilder builer = new ConfigurationBuilder();
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("*********************")
@@ -17,5 +20,41 @@ public class Bot {
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
         // Rest of the code
+        
+    }
+    
+    
+    public static class TweetListener implements StatusListener {
+
+        @Override
+        public void onStatus(Status status) {
+            
+        }
+
+        @Override
+        public void onDeletionNotice(StatusDeletionNotice sdn) {
+            
+        }
+
+        @Override
+        public void onTrackLimitationNotice(int i) {
+            
+        }
+
+        @Override
+        public void onScrubGeo(long l, long l1) {
+            
+        }
+
+        @Override
+        public void onStallWarning(StallWarning sw) {
+            
+        }
+
+        @Override
+        public void onException(Exception excptn) {
+            
+        }
+        
     }
 }
