89fc5c8af46b06af0061a743a4991c2f4d68eaf5
mtotodev05 <mtotodev05@gmail.com>
2018-04-20 17:34:31 +0300

    Optimized code

--- src/main/java/com/dita/dev/twitterbot/Main.java
+++ src/main/java/com/dita/dev/twitterbot/Main.java
@@ -1,17 +1,11 @@
 package com.dita.dev.twitterbot;
-
 import java.io.IOException;
-
-/*
-Kamau
- */
 public class Main {
-
-
     public static void main(String [] args) throws IOException{
        Bot  bot = new Bot();
+       String github_link = "https://github.com/mtotodev05";
+       System.out.println("Twitter Bot [By Brian Kamau] @" +github_link);
         //System.out.println(bot.getBotCredentials().toString()); Try to See if Its reading
         bot.retweetByHash(new String[]{bot.getBotCredentials().get(4)}); //Replace with Hash Tag.
-
     }
 }
